868c7203761416d791d4b9d5ce9d5c3cdce4936a
Andres Hernandez <andres.hernandez2@upr.edu>
2017-04-01 12:00:06 -0400

    Add Part III

--- spark/p3a.py
+++ spark/p3a.py
@@ -1,1 +1,6 @@
 #Code here
+
+sFile = sc.textFile('/home/andres/p3exam1/studentsPR.csv')
+s = sFile.map(lambda x: x.split(','))
+result = s.filter(lambda x: x[5]=='F').filter(lambda x: x[2]=='71381')
+result.collect()
--- spark/p3b.py
+++ spark/p3b.py
@@ -1,1 +1,6 @@
 #Code here
+
+eFile = sc.textFile('/home/andres/p3exam1/escuelasPR.csv')
+e = eFile.map(lambda x: x.split(','))
+result = e.filter(lambda x: x[2]=='Ponce' or x[2]=='Cabo Rojo' or x[2]=='Dorado')
+result.collect()
